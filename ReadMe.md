569df2912a0055df0bbae9dddc86f18556d80e4d
Aytac Kahveci <aytackahveci93@gmail.com>
2023-03-10 23:16:02 +0300

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+# CommonAPI Sample Project
---
+++ scripts/generate_capi_code.sh
@@ -0,0 +1,2 @@
+./../commonapi_core_generator/commonapi-core-generator-linux-x86 -sk fidl/Attributes.fidl
+./../commonapi_someip_generator/commonapi-someip-generator-linux-x86 fidl/Attributes-SomeIP.fdepl
